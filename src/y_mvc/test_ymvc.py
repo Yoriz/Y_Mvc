ffbfc948b8b2ab5b124928a7ef4456eb7dbf9e8e
Dave Wilson <dave.wilson1@ntlworld.com>
2013-03-27 23:16:30 +0000

    *

---
+++ src/y_mvc/test_ymvc.py
@@ -0,0 +1,171 @@
+'''
+Created on 24 Mar 2013
+
+@author: Dave Wilson
+'''
+
+import ymvc
+import unittest
+
+
+class TestOnSignal(unittest.TestCase):
+
+    def setUp(self):
+
+        self.attr1 = None
+        self.attr2 = None
+        self.attr3 = None
+
+    def tearDown(self):
+        unittest.TestCase.tearDown(self)
+
+    @ymvc.onNotifyKw('attr1')
+    def onTestOnSignal(self, attr1):
+        self.attr1 = attr1
+
+    @ymvc.onNotifyKw('attr1', 'attr2', 'attr3')
+    def onTestOnSignalManyKw(self, attr1, attr2, attr3):
+        self.attr1 = attr1
+        self.attr2 = attr2
+        self.attr3 = attr3
+
+    def testMethodHasSignal(self):
+        self.assertEqual(
+            ymvc.SignalNotifyKw('attr1'), self.onTestOnSignal._ySignal)
+
+    def testMethodCallDirect(self):
+        self.onTestOnSignal('testMethodCallDirect')
+        self.assertEqual('testMethodCallDirect', self.attr1)
+
+    def testMethodCallRightSignal(self):
+        kwargs = {ymvc._SIGNAL: ymvc.SignalNotifyKw('attr1'),
+                  'attr1': 'testMethodCallRightSignal'}
+        self.onTestOnSignal(**kwargs)
+        self.assertEqual('testMethodCallRightSignal', self.attr1)
+
+    def testMethodCallWrongSignal(self):
+        kwargs = {ymvc._SIGNAL: ymvc.SignalNotifyKw('wrong'),
+                  'attr1': 'testMethodCallRightSignal'}
+        self.onTestOnSignal(**kwargs)
+        self.assertEqual(None, self.attr1)
+
+    def testMethodCallManyKw(self):
+        kwargs = {ymvc._SIGNAL: ymvc.SignalNotifyKw('attr1', 'attr2', 'attr3'),
+                  'attr1': 'attr1',
+                  'attr2': 'attr2',
+                  'attr3': 'attr3'}
+        self.onTestOnSignalManyKw(**kwargs)
+        self.assertEqual('attr1', self.attr1)
+        self.assertEqual('attr2', self.attr2)
+        self.assertEqual('attr3', self.attr3)
+
+
+class TestBase(unittest.TestCase):
+
+    def setUp(self):
+        self.ymvcBase = ymvc.YmvcBase()
+        self.notifyCalled = False
+        self.attr1 = None
+        self.attr2 = None
+        self.attr3 = None
+
+    def tearDown(self):
+        unittest.TestCase.tearDown(self)
+
+    @ymvc.onNotifyKw('attr1')
+    def onTestOnSignalKw(self, attr1):
+        self.attr1 = attr1
+
+    @ymvc.onNotifyKw('attr2', 'attr1', 'attr3')
+    def onTestOnSignalManyKw(self, attr2, attr1, attr3):
+        self.attr1 = attr1
+        self.attr2 = attr2
+        self.attr3 = attr3
+
+    @ymvc.onNotify('Empty')
+    def onTestOnSignalNotify(self):
+        self.notifyCalled = True
+
+    def testNotifyKw(self):
+        self.ymvcBase.bind(self.onTestOnSignalKw)
+        self.ymvcBase.notifyKw(attr1='TestValue')
+        self.ymvcBase.waitInQueue()
+        self.assertEqual('TestValue', self.attr1)
+
+    def testNotifyManyKw(self):
+        self.ymvcBase.bind(self.onTestOnSignalManyKw)
+        self.ymvcBase.notifyKw(attr1='attr1', attr2='attr2', attr3='attr3')
+        self.ymvcBase.waitInQueue()
+        self.assertEqual('attr1', self.attr1)
+        self.assertEqual('attr2', self.attr2)
+        self.assertEqual('attr3', self.attr3)
+
+    def testNotify(self):
+        self.ymvcBase.bind(self.onTestOnSignalNotify)
+        self.ymvcBase.notify('Empty')
+        self.ymvcBase.waitInQueue()
+        self.assertEqual(True, self.notifyCalled)
+
+
+class TestModel1(ymvc.Model):
+
+    def __init__(self, attr1='StartValue', attr2='StartValue'):
+        super(TestModel1, self).__init__('attr1', 'attr2')
+        self.attr1 = attr1
+        self.attr2 = attr2
+
+
+class TestModel(unittest.TestCase):
+
+    def setUp(self):
+        self.model = TestModel1()
+        self.attr1 = None
+        self.attr2 = None
+
+    @ymvc.onAttr('attr1')
+    def attr1Callback(self, attr1):
+        self.attr1 = attr1
+
+    @ymvc.onAttr('attr2')
+    def attr2Callback(self, attr2):
+        self.attr2 = attr2
+
+    def testSetAttr(self):
+        self.model.attr1 = 'Testing'
+        self.model.waitInQueue()
+        self.assertEqual('Testing', self.model.attr1)
+
+    def testSlotGetAttr(self):
+        self.model.slotGetAttr(self.attr1Callback)
+        self.model.waitInQueue()
+        self.assertEqual(self.model.attr1, self.attr1)
+
+    def testSlotAttrCallbackOnBind(self):
+        self.model.bind(self.attr1Callback)
+        self.model.waitInQueue()
+        self.assertEqual('StartValue', self.attr1)
+
+    def testSetAttrCallback(self):
+        self.model.bind(self.attr1Callback)
+        self.model.attr1 = 'Testing'
+        self.model.waitInQueue()
+        self.assertEqual('Testing', self.attr1)
+
+
+class TestController(unittest.TestCase):
+
+    def setUp(self):
+
+        class Gui(object):
+            def __init__(self):
+                self.view = ymvc.View(self)
+
+        self.gui = Gui()
+
+    def testCreateController(self):
+        controller = ymvc.Controller(self.gui.view)
+        self.assertEqual(self.gui, controller.gui)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ src/y_mvc/ymvc.py
@@ -0,0 +1,160 @@
+'''
+Created on 24 Mar 2013
+
+@author: Dave Wilson
+'''
+
+from y_signal.ysignal import Ysignal
+from functools import wraps
+
+_SIGNAL = 'uniqueSignalKeyName'
+
+
+class SignalNotify(tuple):
+    '''Creates a Notification signal'''
+    def __new__(self, name):
+        return tuple.__new__(self, name)
+
+
+class SignalNotifyKw(tuple):
+    '''Creates a Notification signal with given keywords'''
+    def __new__(self, *keywords):
+        items = list(sorted(keywords))
+        items.insert(0, 'SignalNotifyKeywords')
+        return tuple.__new__(self, items)
+
+
+class SignalAttr(tuple):
+    '''Creates a attribute signal for one or more keywords'''
+    def __new__(self, *keywords):
+        if not keywords:
+            raise AttributeError('At least one keyword required')
+        items = list(keywords)
+        items.insert(0, 'SignalAttr')
+        return tuple.__new__(self, items)
+
+
+def onSignal(signal):
+    '''Signal handler decorator, the decorated method will only be called if
+    the signal argument passed in matches the decorated signal or if no
+    signal argument is passed in at all'''
+    def decorator(target):
+        target._ySignal = signal
+
+        @wraps(target)
+        def wrapper(self, *args, **kwargs):
+            signal_call = kwargs.pop(_SIGNAL, None)
+            if not signal_call or (signal_call == signal):
+                return target(self, *args, **kwargs)
+        return wrapper
+    return decorator
+
+
+def onAttr(Attribute):
+    '''Decorator a slot that binds to a model attribute'''
+    return onSignal(SignalAttr(Attribute))
+
+
+def onNotify(message):
+    '''Decorator a slot that binds to a message notification'''
+    return onSignal(SignalNotify(message))
+
+
+def onNotifyKw(*keywords):
+    '''Decorator a slot that binds to a keywords notification'''
+    return onSignal(SignalNotifyKw(*keywords))
+
+
+class YmvcBase(object):
+    '''YmvcBase object for signal communication in ymvc'''
+    def __init__(self):
+        '''Initialise attributes'''
+        self._ySignal = Ysignal(False)
+
+    def bind(self, slot):
+        '''bind a slot'''
+        self._ySignal.bind(slot)
+
+    def waitInQueue(self):
+        '''wait in the signal queue till current signals are done'''
+        self._ySignal.waitInQueue()
+
+    def waitTillQueueEmpty(self):
+        '''Wait till the queue is empty (not reliable!)'''
+        self._ySignal.waitTillQueueEmpty()
+
+    def notify(self, message):
+        '''Sends a message notification to all slots interested
+        Decorate slots with onNotify(message)'''
+        kwargs = {_SIGNAL: SignalNotify(message)}
+        return self._ySignal.emit(**kwargs)
+
+    def notifyKw(self, **kwargs):
+        '''Sends a keywords notification to all slots interested
+        Decorate slots with onNotifyKw(*keywords)'''
+        kwargs[_SIGNAL] = SignalNotifyKw(*kwargs.keys())
+        return self._ySignal.emit(**kwargs)
+
+
+class View(YmvcBase):
+    '''Communicates from your view to the controller'''
+    def __init__(self, gui):
+        super(View, self).__init__()
+        self.gui = gui
+        self.controller = None
+
+    def setController(self, controller):
+        '''Set the controller related to this view'''
+        self.controller = controller
+
+
+class Model(YmvcBase):
+    '''Contains the data of your application'''
+
+    _signaledAttr = []
+
+    def __init__(self, *attributes):
+        '''Set the attributes you want to observe changes
+        Decorate slots with onAttr(attribute)'''
+        super(Model, self).__init__()
+        self._signaledAttr = attributes
+
+    def bind(self, slot):
+        '''binds a slot if it is an attribute slot emits its value'''
+        super(Model, self).bind(slot)
+        if isinstance(slot._ySignal, SignalAttr):
+            self.slotGetAttr(slot)
+
+    def __setattr__(self, name, value):
+        '''Add a _setattrCall to the queue if listed in _signaledAttr'''
+        if not hasattr(self, name):
+            return YmvcBase.__setattr__(self, name, value)
+
+        if name in self._signaledAttr:
+            future = self._ySignal.signalExe.submit(self._setattrCall,
+                                                    name, value)
+            future.add_done_callback(self._ySignal.slotCheck)
+            return future
+        else:
+            return YmvcBase.__setattr__(self, name, value)
+
+    def _setattrCall(self, name, value):
+        '''Sets an attributes value and then sends a signal of its new value'''
+        YmvcBase.__setattr__(self, name, value)
+        kwargs = {_SIGNAL: SignalAttr(name), name: getattr(self, name)}
+        self._ySignal._emitCall(**kwargs)
+
+    def slotGetAttr(self, slot):
+        '''Emit the attribute for this slot only'''
+        signal, name = slot._ySignal, slot._ySignal[1]
+        kwargs = {_SIGNAL: signal, name: getattr(self, name)}
+        return self._ySignal.emitSlot(slot, **kwargs)
+
+
+class Controller(object):
+    '''Communicates between a view and models'''
+    def __init__(self, view):
+        '''Sets up the view/controller pairing'''
+        self.view = view
+        self.gui = view.gui
+        view.setController(self)
